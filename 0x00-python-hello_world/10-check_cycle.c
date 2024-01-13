13c89ffcb56f76a2d99b53d0a398bf74f7626083
Gerges-Creative <gerges.zechariah@gmail.com>
2024-01-13 10:39:06 -0800

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list has a cycle in it.
+ * @list: a pointer to a pointer of a head of a linked list.
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	/**
+	 * I'm going to use the trotoise/hare algorithm where the tortoise
+	 * will go one node at a time and the hare 2 node at a time and
+	 * if they ever have the same n value of the linked list then there
+	 * is a cycle.
+	 */
+	listint_t *tortoise, *hare;
+
+	tortoise = list;
+	hare = list;
+
+	while (hare != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next;
+		hare = hare->next;
+
+		if (tortoise->n == hare->n)
+			return (1);
+	}
+
+	return (0);
+}
