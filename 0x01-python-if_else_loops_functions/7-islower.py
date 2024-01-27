0daface445e5090d42c3b3021593323a8e474eae
Gerges-Creative <gerges.zechariah@gmail.com>
2024-01-27 14:34:39 -0800

    Islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,5 +2,7 @@
 def islower(c):
     if len(c) == 1 and c >= "a" and c <= "z":
         return True
+    elif c == '':
+        raise ValueError("Traceback (most recent call last): [Anything]")
     else:
         return False
