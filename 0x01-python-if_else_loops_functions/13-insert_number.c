346080fbfa4495aea267c3affb6ff3ee2dd35670
Gerges-Creative <gerges.zechariah@gmail.com>
2024-01-30 14:43:29 -0800

    insert at index

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -32,7 +32,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (*head);
 	}
 
-	while (number > temp->n && temp != NULL)
+	while (number > temp->n && temp->next != NULL)
 	{
 		temp = temp->next;
 		i++;
