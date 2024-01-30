a1f1d50a36341c358c1c3c89b1c68b35c86288c0
Gerges-Creative <gerges.zechariah@gmail.com>
2024-01-30 11:43:05 -0800

    Convert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if i >= 'a' and i <= 'z':
+            i = chr(ord(i) - 32)
+        print('{0:s}'.format(i), end='')
+
+    print('')
+
+
+uppercase("best")
