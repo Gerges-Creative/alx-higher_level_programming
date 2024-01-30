8df65e8b89c2ef01069b8cc8853f1f6d5eb99481
Gerges-Creative <gerges.zechariah@gmail.com>
2024-01-30 13:02:39 -0800

    insert at index

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert node at index nth
+ * @head: poiter that points to the head node
+ * @number: is the number of the nth index to insert node
+ * Return: the address of the new node or NULL if it failes
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *temp, temp2;
+	int i = 0;
+
+	temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	while (i < number - 1 && temp != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (temp == NULL)
+		return (NULL);
+
+	temp2 = temp->next;
+	temp->next = new;
+	new->next = temp2;
+
+	return (new);
+}
