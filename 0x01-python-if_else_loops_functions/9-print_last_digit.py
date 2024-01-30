15f96e60ff8a6a5a090753d345c91a14bfc99cd9
Gerges-Creative <gerges.zechariah@gmail.com>
2024-01-30 12:08:01 -0800

    Last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         last = number % -10
-    last = number % 10
+    else:
+        last = number % 10
     print('{0}'.format(last), end='')
     return last
