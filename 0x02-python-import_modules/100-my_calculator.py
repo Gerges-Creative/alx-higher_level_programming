8a32fe963b3067fe0e2e2fdc98337c3ac08cf20f
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-11 12:27:49 -0800

    Build Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-from calculator_1 import add, sup, mul, div
+from calculator_1 import add, sub, mul, div
 import sys
 argv = sys.argv
 args = len(argv)
-a = int(argv[1])
-b = int(argv[3])
 if __name__ == "__main__":
-    if args != 3:
+    if args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+
 
         if argv[2] == '+':
             print("{0} + {1} = {2}".format(a, b, add(a, b)))
