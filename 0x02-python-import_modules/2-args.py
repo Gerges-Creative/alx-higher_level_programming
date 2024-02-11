4fb4f239b73b5e43b18d16ddcc4a6240e3e75c48
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-11 09:15:03 -0800

    Dynamic Script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if args_n == 0:
         print("0 arguments.")
     elif args_n == 1:
-        print("1 argument:\n1: {}".format(args[1]))
+        print("1 argument:\n1: {0}".format(args[1]))
     elif args_n > 1:
         print("{0} arguments:".format(args_n))
         for i in range(1, args_n + 1):
