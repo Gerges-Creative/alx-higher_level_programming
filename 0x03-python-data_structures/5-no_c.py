d1647803e39046a4d58e3409579dd8b6c42395c2
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-13 08:17:45 -0800

    Remove c & C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    count = my_string.count(c)
+    idxc, idxC = -1, -1
 
     while "c" in my_string or "C" in my_string:
-        my_string.remove("c")
-        my_string.remove("C")
+        idxc = my_string.find("c")
+        idxC = my_string.find("C")
+        if idxc != -1:
+            my_string = my_string[:idxc] + my_string[idxc + 1:]
+        if idxC != -1:
+            my_string = my_string[:idxC] + my_string[idxC + 1:]
 
     return my_string
