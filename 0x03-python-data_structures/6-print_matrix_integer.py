f2bddae9036bb6c8df4b4d2fd76ab471c864fedb
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-13 08:47:34 -0800

    Print Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
+        i = 0
         for idx in matrix[row]:
             print("{:d}".format(idx), end='')
-            if matrix[row][idx + 1] != None:
+            row_len = len(matrix[row])
+            if i < row_len - 1:
                 print(" ", end='')
-
+            i += 1
         print()
