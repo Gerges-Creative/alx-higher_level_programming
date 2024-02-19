2f95c15f40d67423db72c1c2b07d6790aa8bf978
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-18 18:03:43 -0800

    Divise by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    div_list = []
+
+    for i in range(len(my_list)):
+        if (my_list[i] % 2) == 0:
+            my_list[i] = True
+        else:
+            my_list[i] = False
+
+    return div_list
