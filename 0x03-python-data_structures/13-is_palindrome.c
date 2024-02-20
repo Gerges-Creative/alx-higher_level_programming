3c84e315ef4fd620ffd62d37fc6b9a13e2843d20
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-20 06:26:59 -0800

    Linked list Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,7 +14,7 @@ int is_palindrome(listint_t **head)
 	if (*head == NULL)
 		return (1);
 	if (head == NULL)
-		return (0);
+		return (1);
 	node = *head;
 	/* while loop to count how many nodes in the linked list */
 	while (node != NULL)
