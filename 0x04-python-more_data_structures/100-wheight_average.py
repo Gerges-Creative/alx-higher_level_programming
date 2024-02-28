ce5216db6420f9114a1f7c860061e81016e1dc62
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-28 09:20:29 -0800

    Weighted Averages

---
+++ 0x04-python-more_data_structures/100-wheight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+
+    if len(my_list) == 0:
+        return 0
+
+    score = 0
+    weight = 0
+    for i in my_list:
+        score += (i[0] * i[1])
+        weight += i[1]
+
+    weightAverage = score / weight
+
+    return weightedAverage
