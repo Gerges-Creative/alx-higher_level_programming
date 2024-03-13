8c26a11cf715b2d83b4dbe63555c38e7326ac323
Gerges-Creative <gerges.zechariah@gmail.com>
2024-03-13 08:55:18 -0700

    Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- This script displays the average temperatures by city ordered by
+-- temperatures descending
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp;
