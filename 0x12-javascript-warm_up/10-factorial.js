2688575d11910d073cede39579888699f787f31d
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-11 09:19:34 -0700

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const arg = process.argv.slice(2);
+
+if (isNaN(arg[0])) {
+  console.log(1);
+}
+
+function factorial (a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+
+console.log(factorial(arg[0]));
