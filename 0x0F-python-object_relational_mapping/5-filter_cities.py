3ca6b6dbe2a17c73c18c132d0914d568f46de108
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-18 11:25:42 -0700

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-return all table values (table 'states')
+returns table values (table 'states')
 parameters given to script: username, password, database
 """
 
@@ -9,7 +9,7 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    def mysqlconnect(username, password, db_name):
+    def mysqlconnect(username, password, db_name, state_name):
         """
         Establishes a connection to a MySQL database and retrieves
         the IDs of states from a 'states' table.
@@ -33,13 +33,24 @@ if __name__ == '__main__':
         # create cursor to exec queries using SQL
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-        for state in states:
-            if  "N" in state:
-                print("{}".format(state))
+        sql_cmd = """SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states ON cities.state_id=states.id
+        WHERE states.name LIKE %s
+        ORDER BY cities.id ASC"""
 
+        cursor.execute(sql_cmd, ('%' + state_name + '%',))
+        cities = cursor.fetchall()
+
+        city_printed = False
+        for city in cities:
+            if city[2] == state_name:
+                if city_printed:
+                    print(", ", end='')
+                print("{}".format(city[1]), end='')
+                city_printed = True
+        print()
         cursor.close()
         db.close()
 
-    mysqlconnect(sys.argv[1], sys.argv[2], sys.argv[3])
+    mysqlconnect(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
