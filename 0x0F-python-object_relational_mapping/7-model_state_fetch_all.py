8a2b067f9929aecab0ca868d3c9f6ecec2be694b
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-21 14:32:58 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+return all state objects from database via python
+parameters given to script: username, password, database
+"""
 
 import sys
 from model_state import Base, State
@@ -6,6 +10,9 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+
+
+    # make engine for database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
