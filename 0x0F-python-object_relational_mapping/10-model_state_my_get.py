213953821cc2a0a98b982dc33536ff70e0710109
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-22 10:18:00 -0700

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-return first state object from database via python
+return state id given state name; SQL injection free
 parameters given to script: username, password, database
 """
 
@@ -19,11 +19,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(State).order_by(State.id).first()
+    # query python instance in database state id given state name
+    results = session.query(State).filter(State.name == sys.argv[4]).first()
     if results:
-        print("{}: {}".format(results[0], results[1]))
+        print("{}".format(results.id))
     else:
-        print("Nothing")
-
+        print("Not found")
 
     session.close()
