02f7566d0717087ef0d7bf9bc765ddeeccb91cd7
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-28 14:20:00 -0700

    What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    status = response.read()
+
+    print("Body response:")
+    print("    - type:", type(status))
+    print("    - content:", status)  # Keep the response as bytes
+    print("    - utf8 content:", status.decode('utf-8')) # Decode for utf8
