c638c34d97937697734849d894162f9786671be6
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-29 07:36:54 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-
+"""
+given URL & email as params, display response body utf-8, print error codes
+usage: ./3-error_code.py http://0.0.0.0:5000/status_401
+"""
 from sys import argv
-import urllib.parse
 import urllib.request
 
 if __name__ == "__main__":
     url = argv[1]
+    req = urllib.request.Request(url)
 
     try:
-        req = urllib.request.Request(url)
+       with urllib.request.urlopen(req) as response:
+           print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
