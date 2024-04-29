6a8a043550bdc3f4d976fa47f01692cecb62657d
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-29 06:18:18 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
 given URL as parameter, fetch URL and display value from reponse header
-usage: ./1-hbtn_header https://intranet.hbtn.io
+usage: ./1-hbtn_header https://alx-intranet.hbtn.io
 """
 import sys
 import urllib.request
 
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    the_header = response.getheader("X-Request-Id")
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        the_header = response.getheader("X-Request-Id")
 
-print("{}".format(the_header))
+    print("{}".format(the_header))
