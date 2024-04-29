39391cdcc80df5ddae56e142d8433d48865fb83f
Gerges-Creative <gerges.zechariah@gmail.com>
2024-04-29 08:35:11 -0700

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+given URL as parameter, fetch URL and display value from reponse header
+usage: ./5-hbtn_header https://alx-intranet.hbtn.io
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
